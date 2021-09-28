e150a726961b333d38c612e3ae7682e1a06c5e09
Mamidi-Sri-Ranganayaki <sriranganayaki.mamidi@gmail.com>
2021-09-28 14:55:19 +0530

    Kotlin:Day2 Part1

--- week2/kotlinbasics1/app/src/main/java/com/ranganayaki/kotlinbasics/basics/methods.kt
+++ week2/kotlinbasics1/app/src/main/java/com/ranganayaki/kotlinbasics/basics/methods.kt
@@ -4,7 +4,10 @@ fun simple(): Int { // How to return value ?  returning int
     return 10
 }
 
-fun addition(num1: Int, num2: Int): Int { // How to pass parameters and return ? accepting two parameters and returning int
+fun addition(
+    num1: Int,
+    num2: Int
+): Int { // How to pass parameters and return ? accepting two parameters and returning int
     return num1 + num2
 }
 
@@ -12,27 +15,43 @@ fun rtnTyp(): Unit { // What is void ? default return type is Unit = void in jav
     println("Just Call")
 }
 
-fun nTimes( num : Int, n : Int ) = n * num // inline function
+fun nTimes(num: Int, n: Int) = n * num // inline function
 
 fun defaultValues( // indentation would be like this
-    num1 : Int = 30, // default value
-    num2 : Int = 20 //  default value
-) : String {
+    num1: Int = 30, // default value
+    num2: Int = 20 //  default value
+): String {
 //    println(" Num1  $num1 Num2 $num2")
 
     return "Num1 = ${num1}, Num2 = ${num2}"
 }
 
+infix fun Unit.sq(n: Int) = n * n
+
+fun multiple(vararg strs : String) { // variable arguments
+    println(strs.contentToString())
+}
+
 fun main() {
-    val dt = simple()
+
+    multiple("one")
+    multiple("one", "two")
+    multiple("one", "two", "three")
+    multiple("one", "two", "three", "four")
+
+    /*val dt = simple()
     println("Dt is $dt")
     println("Addition is " + addition(12, 3))
     println("Addition is ${addition(12, 3)}")
     rtnTyp()
     println("10 3 Times is ${nTimes(10, 3)}")
-
     println("defaultValues() ${defaultValues()}")
     println("defaultValues(89, 56) ${defaultValues(89, 56)}")
     println("defaultValues(num2 = 4555, num1 = 10) ${defaultValues(num2 = 4555, num1 = 10)}") // named parameters
-    println("defaultValues(num2 = 878) ${defaultValues(num2 = 878)}")
+    println("defaultValues(num2 = 878) ${defaultValues(num2 = 878)}")*/
+
+    println(" Square is ${Unit sq 2}")
+    println(" Square is ${Unit.sq(2)}")
+
+
 }
