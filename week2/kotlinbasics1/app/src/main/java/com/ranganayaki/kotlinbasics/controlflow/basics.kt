f442e48e6c3b929c275f7badd6e05ac77671ecbc
Mamidi-Sri-Ranganayaki <sriranganayaki.mamidi@gmail.com>
2021-09-29 10:17:03 +0530

    condition:if,when

--- week2/kotlinbasics1/app/src/main/java/com/ranganayaki/kotlinbasics/controlflow/basics.kt
+++ week2/kotlinbasics1/app/src/main/java/com/ranganayaki/kotlinbasics/controlflow/basics.kt
@@ -1,19 +1,28 @@
 package com.ranganayaki.kotlinbasics.controlflow
 
-fun demo1() {
-    val os = readLine()
+// for taking input you can use
+// - scanner from java
+// - use kotlins native feature readLine -> you read string
 
+fun demo1() {
+    println("Enter The OS : ")
+    val os = readLine() // take input from user
+    var st = ""
     if(os.equals("10")) {
         println("Oreo")
+        st = "oreo"
     } else if (os.equals("9")) {
         println("Pie")
+        st = "pie"
     } else {
-        print("Android")
+        println("Android")
+        st = "android"
     }
 }
 
 fun demo2() {
-    val os = readLine()
+    println("Enter The OS : ")
+    val os = readLine() // take input from user
     when(os) {
         "10" -> println("Oreo")
         "9" -> print("Pie")
@@ -22,10 +31,31 @@ fun demo2() {
     }
 }
 
-fun main() {
-    println("Enter your Android Version")
-    demo1()
-    println("Enter your Android Version")
-    demo2()
+fun demo3() {
+    println("Enter Name")
+    val nm = readLine()
 
+    val info = if(nm.equals("android")) 11 else 30
+    val dt = when(nm){
+        "123" -> 123
+        else -> 23
+    }
+
+    println("Info $info, Dt $dt")
+}
+
+fun demo4() {
+    val safeNums = intArrayOf(45, 89, 50)
+
+    val num = 10
+
+    when(num) {
+        in 1..10 -> println("it is in range")
+        !in 96..100 -> println("it is not in safe range")
+        in safeNums -> println("it is in safe range")
+        else -> println("invalid number")
+    }
+}
+
+fun main() {
 }
