91a8fc5348e87a5f208288501956a2529da9745b
Mamidi-Sri-Ranganayaki <sriranganayaki.mamidi@gmail.com>
2021-10-05 09:41:24 +0530

    rxkotlin:basics

--- week2/kotlinbasics1/app/src/main/java/com/ranganayaki/kotlinbasics/asyncprog/rx-exts.kt
+++ week2/kotlinbasics1/app/src/main/java/com/ranganayaki/kotlinbasics/asyncprog/rx-exts.kt
@@ -3,12 +3,14 @@ package com.ranganayaki.kotlinbasics.asyncprog
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 
 /*
 implementation 'io.reactivex.rxjava3:rxkotlin:3.0.0'
 implementation 'io.reactivex.rxjava3:rxandroid:3.0.0'
  */
 private fun obs1() {
+    // cold and hot
     val obs = Observable.create<String> {
         // this loop is simulation of going to fb
         // and pulling data back
@@ -25,7 +27,7 @@ private fun obs1() {
     }
 
     obs
-        //.observeOn(AndroidSchedulers.mainThread()) // kindly observer on main thread
+        .observeOn(AndroidSchedulers.mainThread()) // kindly observer on main thread
         .subscribeOn(Schedulers.computation()) // subscribe on worker thread
         .doOnNext { str -> println(str) } // it will be called when observer calls onNext
         .doOnError { th -> println(th.toString()) } // it will be called when observer calls onError
@@ -33,6 +35,22 @@ private fun obs1() {
         .subscribe()
 }
 
+private fun obs2() {
+    Observable.interval(1300, TimeUnit.MILLISECONDS)
+        .subscribeOn(Schedulers.computation())
+        .doOnNext { println(it) }
+//        .doOnError {  }
+//        .doOnComplete {  }
+        .subscribe()
+
+    /*Observable.interval(1300, TimeUnit.MILLISECONDS)
+        .subscribe(
+            { }, // doOnNext
+            { },  // doOnError
+            { } // doOnComplete
+        )*/
+}
+
 fun main() {
-    obs1()
+    obs2()
 }
