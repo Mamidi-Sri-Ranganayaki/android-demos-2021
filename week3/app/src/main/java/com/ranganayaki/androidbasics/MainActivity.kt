466163afd7e49332e65a556a1d3429c07c612e3d
Mamidi-Sri-Ranganayaki <sriranganayaki.mamidi@gmail.com>
2021-10-05 16:16:47 +0530

    android:findviewbyId

--- week3/app/src/main/java/com/ranganayaki/androidbasics/MainActivity.kt
+++ week3/app/src/main/java/com/ranganayaki/androidbasics/MainActivity.kt
@@ -2,10 +2,30 @@ package com.ranganayaki.androidbasics
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+/*
+An activity is a single, focused thing that the user can do.
+Almost all activities interact with the user, so the Activity class takes care of
+creating a window for you in which you can place your UI with setContentView.
+
+There are two methods almost all subclasses of Activity will implement:
+onCreate is where you initialize your activity.
+Most importantly, here you will usually call setContentView(int) with a layout resource defining your UI,
+and using findViewById to retrieve the widgets in that UI that you need to interact with programmatically.
+*/
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //resource.folder.file_name
+        //resource = R, folder = drawable, file_name = ic_launcher_background
+//        R.drawable.ic_launcher_background
+//        R.mipmap.ic_launcher
+        setContentView(R.layout.activity_main)  // it parses xml and creates objects for widgets ans store it in memory
+
+        val txkajddfjh = findViewById<TextView>(R.id.txVw)
+
+        val sdsjdfgsjdg = findViewById<Button>(R.id.btOk)
     }
 }
