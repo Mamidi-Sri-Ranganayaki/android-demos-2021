a4dda77669b6b1ed4990a38711780ca4c0d1d793
Mamidi-Sri-Ranganayaki <sriranganayaki.mamidi@gmail.com>
2021-10-11 11:04:14 +0530

    Jetpack Compose:modifier

--- week4/app/src/main/java/com/ranganayaki/composebasics/ui.kt
+++
@@ -1,9 +0,0 @@
-package com.ranganayaki.composebasics
-
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-
-@Composable
-fun SimpleText() {
-    Text(text = "Welcome To Jetpack Compose")
-}
