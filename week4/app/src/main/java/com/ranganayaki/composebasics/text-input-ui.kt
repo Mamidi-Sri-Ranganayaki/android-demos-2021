ba8c66fc57a5fb0d882342d78f8df91cbdd004c2
Mamidi-Sri-Ranganayaki <sriranganayaki.mamidi@gmail.com>
2021-10-11 11:28:15 +0530

    compose:input state management

---
+++ week4/app/src/main/java/com/ranganayaki/composebasics/text-input-ui.kt
@@ -0,0 +1,26 @@
+package com.ranganayaki.composebasics
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+class InputDemo {
+    companion object {
+
+        @Composable
+        fun NormalInput() {
+
+            var text by remember { mutableStateOf("Hello") }
+
+            TextField(
+                value = text,
+                onValueChange = { text = it },
+                label = { Text("Label") },
+                modifier = Modifier.padding(top = 10.dp)
+            )
+        }
+    }
+}
