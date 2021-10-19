9211f3667e4dbbece4a504eb253afc95c86999dc
Mamidi-Sri-Ranganayaki <sriranganayaki.mamidi@gmail.com>
2021-10-19 17:12:55 +0530

    pre navigation demo

--- week5/casestudies/app/src/main/java/com/ranganayaki/myapplication/oldnav/OldNavActivity.kt
+++ week5/casestudies/app/src/main/java/com/ranganayaki/myapplication/oldnav/OldNavActivity.kt
@@ -2,11 +2,20 @@ package com.ranganayaki.myapplication.oldnav
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
 import com.ranganayaki.myapplication.R
 
 class OldNavActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_old_nav)
+    }
+
+    fun loadFragment( frag : Fragment) {
+        supportFragmentManager.commit {
+            replace(R.id.fcv, frag)
+            addToBackStack(frag.javaClass.canonicalName)
+        }
     }
 }
