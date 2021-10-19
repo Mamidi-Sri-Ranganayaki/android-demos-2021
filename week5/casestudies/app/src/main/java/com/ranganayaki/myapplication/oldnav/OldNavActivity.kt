abf83b22d464d5ec590f7a2de2f39b2ffd210754
Mamidi-Sri-Ranganayaki <sriranganayaki.mamidi@gmail.com>
2021-10-19 16:25:08 +0530

    passing data in argument style

---
+++ week5/casestudies/app/src/main/java/com/ranganayaki/myapplication/oldnav/OldNavActivity.kt
@@ -0,0 +1,12 @@
+package com.ranganayaki.myapplication.oldnav
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ranganayaki.myapplication.R
+
+class OldNavActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_old_nav)
+    }
+}
