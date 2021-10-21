066e1790b19718258af04bd36ce41c8dd9bddf2f
Mamidi-Sri-Ranganayaki <sriranganayaki.mamidi@gmail.com>
2021-10-21 17:02:30 +0530

    basic setup

--- week5/jetpack-compose-navigation/app/src/main/java/com/ranganayaki/composenavigation/MainActivity.kt
+++ week5/jetpack-compose-navigation/app/src/main/java/com/ranganayaki/composenavigation/MainActivity.kt
@@ -5,16 +5,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+//import androidx.compose.material.MaterialTheme
+//import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+//import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.ranganayaki.composenavigation.ui.theme.ComposeNavigationTheme
+//import com.ranganayaki.composenavigation.ui.theme.ComposeNavigationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
