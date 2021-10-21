ab8fb37f1d4a6b1544d39156fe787a99d2820921
Mamidi-Sri-Ranganayaki <sriranganayaki.mamidi@gmail.com>
2021-10-21 11:28:10 +0530

    navigation drawer

--- week5/jetpack-navigation-graph/app/src/main/java/com/ranganayaki/jetpacknavigationgraph/hamman/NvDrActivity.kt
+++ week5/jetpack-navigation-graph/app/src/main/java/com/ranganayaki/jetpacknavigationgraph/hamman/NvDrActivity.kt
@@ -11,6 +11,7 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
 import com.ranganayaki.jetpacknavigationgraph.R
 import com.ranganayaki.jetpacknavigationgraph.databinding.ActivityNvDrBinding
 //import com.ranganayaki.jetpacknavigationgraph.hamman.databinding.ActivityNvDrBinding
@@ -34,7 +35,9 @@ class NvDrActivity : AppCompatActivity() {
         }
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
-        val navController = findNavController(R.id.nav_host_fragment_content_nv_dr)
+
+        val fragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_content_nv_dr) as NavHostFragment
+        val navController = fragment.navController
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
