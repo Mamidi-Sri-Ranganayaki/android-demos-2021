3b355c051e1637c513f8b1b22af899fd5d4c48f7
Mamidi-Sri-Ranganayaki <sriranganayaki.mamidi@gmail.com>
2021-10-26 14:29:53 +0530

    case study architecture

---
+++ week6/case-study/app/src/main/java/com/ranganayaki/expensemanager/ExpenseFragment.kt
@@ -0,0 +1,63 @@
+package com.ranganayaki.expensemanager
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import com.ranganayaki.expensemanager.databinding.FragmentExpenseBinding
+//import com.ranganayaki.expensemanager.databinding.FragmentSecondBinding
+
+//**
+/* A simple [Fragment] subclass as the second destination in the navigation.
+*/
+class ExpenseFragment : Fragment() {
+
+    private val viewModel: ExpenseViewModel by activityViewModels()
+
+    private var _binding: FragmentExpenseBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentExpenseBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val dataSource = listOf("Tea", "Coffee", "Vada Pav")
+        val adapter = ArrayAdapter(
+            requireContext(),
+            android.R.layout.simple_expandable_list_item_1,
+            dataSource
+        )
+        binding.expIc.adapter = adapter
+        binding.btSv.setOnClickListener {
+            viewModel.createExpense(
+                Expense(
+                    12,
+                    binding.expNm.text.toString(),
+                    binding.expAmt.text.toString().toDouble(),
+                    R.drawable.ic_plc
+                )
+            )
+            findNavController().popBackStack()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ week6/case-study/app/src/main/java/com/ranganayaki/expensemanager/ExpenseListFragment.kt
@@ -0,0 +1,58 @@
+package com.ranganayaki.expensemanager
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.ranganayaki.expensemanager.databinding.FragmentExpenseListBinding
+
+class ExpenseListFragment : Fragment() {
+
+    private val viewModel: ExpenseViewModel by activityViewModels()
+
+    private var _binding: FragmentExpenseListBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentExpenseListBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Toast.makeText(requireContext(), "Called", Toast.LENGTH_SHORT).show()
+
+        val adapter = ExpenseListAdapter(
+            requireContext(),
+            viewModel.getExpenses()
+        )
+
+        binding.recExpLst.adapter = adapter
+
+        val layoutManager = LinearLayoutManager(requireContext())
+        binding.recExpLst.layoutManager = layoutManager
+        binding.recExpLst.addItemDecoration(
+            DividerItemDecoration(
+                requireContext(),
+                layoutManager.orientation
+            )
+        )
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
