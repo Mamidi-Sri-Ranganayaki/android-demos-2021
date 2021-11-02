45a271dd1f4f0666225bc6a8290dad6d36dec112
Mamidi-Sri-Ranganayaki <sriranganayaki.mamidi@gmail.com>
2021-11-02 11:38:29 +0530

    Work Manager

---
+++ week7/workermanager/app/src/main/java/com/ranganayaki/wrkmgr/worker/uploader.kt
@@ -0,0 +1,20 @@
+package com.ranganayaki.wrkmgr.worker
+
+import android.content.Context
+import android.util.Log
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+
+class FileUploadWork(
+    private val context : Context,
+    private val params : WorkerParameters
+) : Worker(context, params) {
+
+    override fun doWork(): Result {
+        Log.i("@ani", "Doing File Upload")
+        val nm = inputData.getString("KEY_FILE_NAME")
+        val sz = inputData.getLong("KEY_FILE_SIZE", -1L)
+        Log.i("@ani", "Name: $nm, Size: $sz")
+        return Result.retry()
+    }
+}
---
+++ week7/workermanager/app/src/main/java/com/ranganayaki/wrkmgr/worker/work-requests.kt
@@ -0,0 +1,37 @@
+package com.ranganayaki.wrkmgr.worker
+
+import android.content.Context
+import androidx.work.*
+import java.util.concurrent.TimeUnit
+
+
+internal fun scheduleOnce(context : Context) {
+    val uploadRequest = OneTimeWorkRequestBuilder<FileUploadWork>().build()
+    WorkManager.getInstance(context).enqueue(uploadRequest)
+}
+
+internal fun scheduleRepeating(context : Context) {
+    val constraints = Constraints.Builder()
+        .setRequiredNetworkType(NetworkType.UNMETERED) // no restriction on data
+        .setRequiresCharging(true)
+        .build()
+
+    val uploadRequest = PeriodicWorkRequestBuilder<FileUploadWork>(
+        15,
+        TimeUnit.MINUTES
+    ).setConstraints(constraints).build()
+
+    WorkManager.getInstance(context).enqueue(uploadRequest)
+}
+
+internal fun scheduleOnceWithData(context: Context) {
+    val uploadRequest = OneTimeWorkRequestBuilder<FileUploadWork>()
+        .setInputData(
+            workDataOf(
+                "KEY_FILE_NAME" to "abc.txt",
+                "KEY_FILE_SIZE" to 10L
+            )
+        )
+        .build()
+    WorkManager.getInstance(context).enqueue(uploadRequest)
+}
