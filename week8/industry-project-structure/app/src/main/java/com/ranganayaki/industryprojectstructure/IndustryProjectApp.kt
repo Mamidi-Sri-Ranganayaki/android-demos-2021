9d6cf614ab770690b3cf1014e4723fa98ff2975e
Mamidi-Sri-Ranganayaki <sriranganayaki.mamidi@gmail.com>
2021-11-12 15:41:09 +0530

    setup: navigation and fab

---
+++ week8/industry-project-structure/app/src/main/java/com/ranganayaki/industryprojectstructure/IndustryProjectApp.kt
@@ -0,0 +1,8 @@
+package com.ranganayaki.industryprojectstructure
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class IndustryProjectApp : Application() {
+}
